d08710a05e404bcc62ea1f4c4c0683fe291078e2
sddanila <sddanila@yahoo.co.uk>
2019-02-12 03:42:07 +0000

    adding soltuion to the getRepoContributors and downloadIamage functions to download-avatars.js

--- download-avatars.js
+++ download-avatars.js
@@ -1,28 +1,51 @@
 var request = require('request');
 var token = require('./secrets.js');
+var fs = require('fs');
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-    var options = {
-      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-      headers: {
-        'User-Agent': 'request',
-        'Authorization': 'token ' + token.GITHUB_TOKEN
-      }
-    };
-  
-    request(options, function(err, res, body) {
-      const urlArr = JSON.parse(body);
-      console.log(urlArr);
-            urlArr.forEach(function(element){
-                console.log(element.avatar_url)
-            });
-        cb(err, body);
-    });
-  }
 
-  getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
-  });
+var urlArr = [];
+var loginArr = [];
+function getRepoContributors(repoOwner, repoName, cb) {
+    if(!fs.existsSync('./avatars')){
+        fs.mkdirSync('./avatars')
+    }
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': 'token ' + token.GITHUB_TOKEN
+        }
+    };
+    
+    request(options, function(err, res, body) {
+        if (err){
+            console.log(err);
+        }
+        var parsedArr = JSON.parse(body);
+        // console.log(parsedArr);
+        // console.log(parsedArr);
+        parsedArr.forEach(function(element){
+            urlArr.push(element.avatar_url);
+            loginArr.push(element.login);
+        });
+        // console.log(urlArr);
+        // console.log(loginArr);
+        for(var i = 0; i < urlArr.length; i++){
+            var filePath = './avatars/' + loginArr[i] + '.jpg';
+            console.log(filePath);
+            cb(urlArr[i], filePath);
+        }
+    });
+}
+    
+    
+function downloadImageByURL(url, filePath){
+        request.get(url)
+        .pipe(fs.createWriteStream(filePath))
+}
+
+
+getRepoContributors("jquery", "jquery", downloadImageByURL)
